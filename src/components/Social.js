c8ae8f8c14aa4db4a36d444f5bc0d30b1b4b7cc5
ADENIYI LUQMAN <lokobanti90@gmail.com>
2021-10-20 18:38:58 +0100

    social handle markup complete

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,11 +1,37 @@
 const Social = () => {
-    return ( 
+    return (
         <div className="social">
-            Step 2/3
-            Social
-
+            <p className="step">Step 2/3</p>
+            <div>
+                <h1>Social Handles</h1>
+                <p className="select-method">Enter you business social media handles</p>
+            </div>
+            <form>
+                <label htmlFor="">Choose your Abeg Tag(required)</label>
+                <div className="input-container">
+                    <input type="text" name="Abeg" placeholder="@" />
+                </div>
+                <div className="d-flex-con2">
+                    <div className="ig-div">
+                        <label htmlFor="">Instagram</label>
+                        <div className="input-container-flex">
+                            <input type="text" name="instagram" placeholder="@"/>
+                        </div>
+                    </div>
+                    <div className="twitter-div">
+                        <label htmlFor="">Twitter</label>
+                        <div className="input-container-flex">
+                            <input type="text" name="twitter" placeholder="@"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="continue-btn">
+                    <button>Confirm Social Handles</button>
+                </div>
+            </form>
+            
         </div>
-     );
+    );
 }
- 
+
 export default Social;
--- src/components/Verify.js
+++ src/components/Verify.js
@@ -23,59 +23,61 @@ const Verify = () => {
                 <button onClick={handleAcctNumberToggle} className={isSelected ? 'btn' : 'btn-border-blue'}>Personal Account Number</button>
             </div>
             <div className="bvn-container">
-                {isSelected ? (<div>
-                    <label htmlFor="">Bank Verification Number(11-digits)</label>
-                    <div className="input-container">
-                        <input type="number" name="BVN" />
-                    </div>
-                </div>) :
-                    (<div className="d-flex-con">
-                        <div className="account-number-div">
-                            <label htmlFor="">Account Number</label>
-                            <div className="input-container">
-                                <input type="number" name="Acct-no" />
-                            </div>
+            {isSelected ? (
+            <div>
+                <label htmlFor="">Bank Verification Number(11-digits)</label>
+                <div className="input-container">
+                    <input type="number" name="BVN" />
+                </div>
+                <div className="display-info">
+                    <div className="display-description">
+                        <div className="lock-container">
+                            <FontAwesomeIcon className="lock" icon={faLock} />
+                            <p>Why we need your BVN</p>
                         </div>
-                        <div className="select-bank-div">
-                            <label htmlFor="">Select Bank</label>
-                            <div className="input-container">
-                                <select name="" id="">
-                                    <option value=""></option>
-                                    <option value="Gtbank">Guaranty Bank</option>
-                                    <option value="Accessbank">Access Bank</option>
-                                </select>
-                            </div>
+                        <div className="show-container" onClick={handleHide}>
+                            <p>{isShowing ? 'Hide' : 'Show'}</p>
+                            <FontAwesomeIcon className="arrow-down" icon={isShowing ? faAngleUp : faAngleDown} />
                         </div>
                     </div>
-                    )}
-            </div>
-            <div className="display-info">
-                <div className="display-description">
-                    <div className="lock-container">
-                        <FontAwesomeIcon className="lock" icon={faLock} />
-                        <p>Why we need your BVN</p>
-                    </div>
-                    <div className="show-container" onClick={handleHide}>
-                        <p>{isShowing ? 'Hide' : 'Show'}</p>
-                        <FontAwesomeIcon className="arrow-down" icon={isShowing ? faAngleUp : faAngleDown} />
-                    </div>
-                </div>
-                <div className={isShowing ? "bvn-access-info" : 'd-none'}>
-                    <p>We only need access to your:</p>
-                    <ul>
-                        <li>Full Name</li>
-                        <li>Phone Number</li>
-                        <li>Date of Birth</li>
-                    </ul>
-                    <div className="bvn-footer">
-                        <p>Your BVN does not give us access to your bank accounts or transactions</p>
+                    <div className={isShowing ? "bvn-access-info" : 'd-none'}>
+                        <p>We only need access to your:</p>
+                        <ul>
+                            <li>Full Name</li>
+                            <li>Phone Number</li>
+                            <li>Date of Birth</li>
+                        </ul>
+                        <div className="bvn-footer">
+                            <p>Your BVN does not give us access to your bank accounts or transactions</p>
+                        </div>
                     </div>
                 </div>
+            </div>) 
+            :(<div className="d-flex-con">
+                    <div className="account-number-div">
+                        <label htmlFor="">Account Number</label>
+                        <div className="input-container">
+                            <input type="number" name="Acct-no" />
+                        </div>
+                    </div>
+                    <div className="select-bank-div">
+                        <label htmlFor="">Select Bank</label>
+                        <div className="input-container">
+                            <select name="" id="">
+                                <option value=""></option>
+                                <option value="Gtbank">Guaranty Bank</option>
+                                <option value="Accessbank">Access Bank</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                )}
             </div>
-            {isShowing ? (
-                <div className="continue-btn">
-                    <button>Complete</button>
-                </div>) : ''}
+            
+            
+            <div className="continue-btn">
+                <button>Complete</button>
+            </div>
         </div>
     );
 }
