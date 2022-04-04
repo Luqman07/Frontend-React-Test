f370ace83f4dc660ee9517e97e3e8acd420e85c0
ADENIYI LUQMAN <lokobanti90@gmail.com>
2022-04-04 14:03:08 +0100

    changed the home nav to the github name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = ({isCheckOne, isCheckTwo, isCheckThree}) => {
                 <button>Logout</button>
             </div>
             <ul>
-                <li><NavLink className="navlink" activeClassName="selected" to="/" exact ><button>{isCheckOne ? (<span style={{color: 'green'}}>&#10003;</span>) : "1"}</button><span className="text">Verify Account</span></NavLink></li>
+                <li><NavLink className="navlink" activeClassName="selected" to="/Frontend-React-Test" exact ><button>{isCheckOne ? (<span style={{color: 'green'}}>&#10003;</span>) : "1"}</button><span className="text">Verify Account</span></NavLink></li>
                 <li><NavLink className="navlink" activeClassName="selected" to="/social"><button>{isCheckTwo ? (<span style={{color: 'green'}}>&#10003;</span>) : "2"}</button><span className="text">Social Handles</span></NavLink></li>
                 <li><NavLink className="navlink" activeClassName="selected" to="/business"><button>{isCheckThree ? (<span style={{color: 'green'}}>&#10003;</span>) : "3"}</button><span className="text">Business Category</span></NavLink></li>
             </ul>
--- src/components/Verify.js
+++ src/components/Verify.js
@@ -90,3 +90,4 @@ const Verify = ({setIsCheckOne}) => {
 }
 
 export default Verify;
+
